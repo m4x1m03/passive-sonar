1cab1c2d1d654ab6ad8c6ba054eb99ba3d57dcfe
Maxim Mouget <89623028+m4x1m03@users.noreply.github.com>
2026-01-14 09:56:48 -0600

    Update to simulate my linear 4 mic array

--- test_mic_arr.py
+++ test_mic_arr.py
@@ -1,47 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-DOA Algorithms
-==============
-
-This example demonstrates how to use the DOA object to perform direction of arrival
-finding in 2D using one of several algorithms
-
-- MUSIC [1]_
-- SRP-PHAT [2]_
-- CSSM [3]_
-- WAVES [4]_
-- TOPS [5]_
-- FRIDA [6]_
-
-.. [1] R. Schmidt, *Multiple emitter location and signal parameter estimation*,
-    IEEE Trans. Antennas Propag., Vol. 34, Num. 3, pp 276--280, 1986
-
-.. [2] J. H. DiBiase, J H, *A high-accuracy, low-latency technique for talker localization
-    in reverberant environments using microphone arrays*, PHD Thesis, Brown University, 2000
-
-.. [3] H. Wang, M. Kaveh, *Coherent signal-subspace processing for the detection and
-    estimation of angles of arrival of multiple wide-band sources*, IEEE Trans. Acoust.,
-    Speech, Signal Process., Vol. 33, Num. 4, pp 823--831, 1985
-
-.. [4] E. D. di Claudio, R. Parisi, *WAVES: Weighted average of signal subspaces for
-    robust wideband direction finding*, IEEE Trans. Signal Process., Vol. 49, Num. 10,
-    2179--2191, 2001
-
-.. [5] Y. Yeo-Sun, L. M. Kaplan, J. H. McClellan, *TOPS: New DOA estimator for wideband
-    signals*, IEEE Trans. Signal Process., Vol. 54, Num 6., pp 1977--1989, 2006
-
-.. [6] H. Pan, R. Scheibler, E. Bezzam, I. Dokmanić, and M. Vetterli, *FRIDA:
-    FRI-based DOA estimation for arbitrary array layouts*, Proc. ICASSP,
-    pp 3186-3190, 2017
-
-In this example, we generate some random signal for a source in the far field
-and then simulate propagation using a fractional delay filter bank
-corresponding to the relative microphone delays.
-
-Then we perform DOA estimation and compare the errors for different algorithms
-
-"""
-
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.signal import fftconvolve
@@ -57,6 +13,12 @@ azimuth = 61.0 / 180.0 * np.pi  # 60 degrees
 distance = 3.0  # 3 meters
 dim = 2  # dimensions (2 or 3)
 room_dim = np.r_[10.0, 10.0]
+valid_algorithms = [
+    "SRP",
+    "MUSIC",
+    "CSSM",
+    "WAVES",
+]
 
 # Use AnechoicRoom or ShoeBox implementation. The results are equivalent because max_order=0 for both.
 # The plots change a little because in one case there are no walls.
@@ -86,7 +48,7 @@ source_signal = np.random.randn((nfft // 2 + 1) * nfft)
 aroom.add_source(source_location, signal=source_signal)
 
 # We use a circular array with radius 15 cm # and 12 microphones
-R = pra.circular_2D_array(room_dim / 2, 12, 0.0, 0.15)
+R = pra.linear_2D_array(room_dim/2, 4, 0, 0.0635)
 aroom.add_microphone_array(pra.MicrophoneArray(R, fs=aroom.fs))
 
 # run the simulation
@@ -101,24 +63,63 @@ X = np.array(
     ]
 )
 
+# PLOTTING
+def plot_doa_polar(azimuth_est, azimuth_true=None, title="DOA"):
+    """
+    azimuth_est : scalar or array-like (radians)
+    azimuth_true: scalar (radians) or None
+    """
+    azimuth_est = np.atleast_1d(azimuth_est)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, polar=True)
+
+    # Estimated DOAs (red)
+    for az in azimuth_est:
+        ax.plot([az, az], [0, 1.0], "r-", linewidth=2)
+
+    # True DOA (green)
+    if azimuth_true is not None:
+        ax.plot([azimuth_true, azimuth_true], [0, 1.0], "g--", linewidth=2)
+
+    ax.set_rmax(1.0)
+    ax.set_rticks([])
+    ax.set_theta_zero_location("E")   # 0° = +x axis
+    ax.set_theta_direction(1)          # CCW positive
+    ax.set_title(title)
+
+    return ax
+
+def mirrored_azimuth(az):
+    az = np.atleast_1d(az)
+    return np.concatenate([az, -az])
+
+
 ##############################################
 # Now we can test all the algorithms available
 algo_names = sorted(pra.doa.algorithms.keys())
 
-for algo_name in algo_names:
-    # Construct the new DOA object
-    # the max_four parameter is necessary for FRIDA only
+for algo_name in valid_algorithms:
     doa = pra.doa.algorithms[algo_name](R, fs, nfft, c=c, max_four=4)
-
-    # this call here perform localization on the frames in X
     doa.locate_sources(X, freq_bins=freq_bins)
 
-    doa.polar_plt_dirac()
-    plt.title(algo_name)
+    # Plot without pyroomacoustics
+    plot_doa_polar(
+        azimuth_est=mirrored_azimuth(doa.azimuth_recon),
+        azimuth_true=azimuth,
+        title=f"{algo_name} (front/back)",
+    )
 
-    # doa.azimuth_recon contains the reconstructed location of the source
     print(algo_name)
-    print("  Recovered azimuth:", doa.azimuth_recon / np.pi * 180.0, "degrees")
-    print("  Error:", circ_dist(azimuth, doa.azimuth_recon) / np.pi * 180.0, "degrees")
+    print(
+        "  Recovered azimuth:",
+        np.atleast_1d(doa.azimuth_recon) / np.pi * 180.0,
+        "degrees",
+    )
+    print(
+        "  Error:",
+        circ_dist(azimuth, doa.azimuth_recon) / np.pi * 180.0,
+        "degrees",
+    )
 
 plt.show()

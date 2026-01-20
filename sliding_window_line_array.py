242321a39c8495ad772f97c90eaef99df42a4288
Maxim Mouget <89623028+m4x1m03@users.noreply.github.com>
2026-01-20 10:56:04 -0600

    drops frame with power under a certain threshold

--- sliding_window_line_array.py
+++ sliding_window_line_array.py
@@ -24,6 +24,7 @@ chunk_duration = 0.1  # seconds
 chunk_size = int(chunk_duration * fs)
 hop_size = chunk_size // 2
 valid_algorithms = "SRP"
+threshold_peak = 1.75 # used for filtering out the silent chunks (static but need to make it dynamic in the future)
 
 # -----------------------------
 # MICROPHONE ARRAY GEOMETRY
@@ -62,12 +63,14 @@ signals = np.stack(signals, axis=0)
 
 
 # -----------------------------
-# Compute the STFT frames needed
+# Main DOA loop
 # -----------------------------
 num_samples = signals.shape[1]
 num_chunks = (num_samples - chunk_size) // hop_size + 1
 times = []
 azimuths = []
+valid_measurement = []
+
 
 doa = pra.doa.algorithms[valid_algorithms](R, fs, nfft, c=c, max_four=4)
 
@@ -88,10 +91,12 @@ for i in range(num_chunks):
     doa.locate_sources(X_chunk, freq_bins=freq_bins)
 
 
-    if doa.azimuth_recon.size == 0:
+    if doa.azimuth_recon.size == 0 or doa.grid.values.max() < threshold_peak:
         az = np.nan
+        valid_measurement.append(False)
     else:
         az = doa.azimuth_recon[0]
+        valid_measurement.append(True)
 
     azimuths.append(az)
     times.append(start / fs)
@@ -146,16 +151,22 @@ for az in azimuths:
 
 filtered_azimuths = []
 
-for az in azimuths:
-    kf.predict()
-    if not np.isnan(az):
-        # Innovation with angle wrapping
+for az, valid in zip(azimuths, valid_measurement):
+    if valid:
+        kf.predict()
+
         z = np.array([[az]])
         y = wrap_angle(z[0, 0] - kf.x[0, 0])
         kf.update(np.array([[kf.x[0,0] + y]]))
 
-    kf.x[0,0] = wrap_angle(kf.x[0,0])
-    filtered_azimuths.append(kf.x[0,0])
+        kf.x[0, 0] = wrap_angle(kf.x[0, 0])
+        filtered_azimuths.append(kf.x[0, 0])
+    else:
+        # Peak is under threshold so don't output a prediction
+        filtered_azimuths.append(np.nan)
+        # Inflate uncertainty by 5% for every chunk with no signal
+        kf.P *= 1.05
+
 
 filtered_azimuths = np.array(filtered_azimuths)
 
@@ -173,7 +184,7 @@ def forward_azimuth(az):
         return az
 
 # -----------------------------
-# Plot DOA from SRP and animated
+# Plot DOA from SRP and animated (prototype so fine with matplotlib but need to upgrade)
 # -----------------------------
 azimuths = np.array(azimuths)
 times = np.array(times)
